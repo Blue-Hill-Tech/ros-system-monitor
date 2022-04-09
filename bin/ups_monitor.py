b6223b3a21e1ee5579718c574a010cb56d0d5b82
shuo <shuo@bluehill.coffee>
2022-04-09 15:41:40 -0700

    Added battery state report to cloud node

--- bin/ups_monitor.py
+++ bin/ups_monitor.py
@@ -15,15 +15,18 @@ def ups_monitor():
 
     while not rospy.is_shutdown():
         try:
-            bat_response = subprocess.check_output(["upower -i $(upower -e | grep 'ups') | grep -E \"percentage\""], shell=True)
+            percentage_response = subprocess.check_output(["upower -i $(upower -e | grep 'ups') | grep -E \"percentage\""], shell=True)
+            state_response = subprocess.check_output(["upower -i $(upower -e | grep 'ups') | grep -E \"state\""], shell=True)
         except subprocess.CalledProcessError as e:
             print("No UPS info obtained. Exiting...")
             sys.exit(0)
-        percentage = float(bat_response.decode("utf-8").replace('\n','').replace(' ', '').split(":")[1].strip('%'))
+        percentage = float(percentage_response.decode("utf-8").replace('\n','').replace(' ', '').split(":")[1].strip('%'))
+        state = state_response.decode("utf-8").replace('\n','').replace(' ', '').split(":")[1]
         bat_stat = DiagnosticStatus()
         bat_stat.name = "UPS Usage"
-        bat_stat.values = [KeyValue(key = 'UPS Percentage',
-                               value = str(percentage))]
+        bat_stat.values = [KeyValue(key = 'UPS Percentage', value = str(percentage)),
+                           KeyValue(key = 'UPS State', value = state)]
+
         msg = DiagnosticArray()
         msg.header.stamp = rospy.get_rostime()
         msg.status = [bat_stat]
